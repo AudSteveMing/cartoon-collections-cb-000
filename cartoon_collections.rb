624995a990fca334055a35613e1400e3ad2c7b9d
Steve Cordova <cordovas@bc.edu>
2020-07-17 19:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,26 +1,42 @@
-def roll_call_dwarves(array)
-  array.each.with_index(1) do |index, value|
-    puts "#{value} #{index}"
+# def roll_call_dwarves(array)
+#   array.each.with_index(1) do |index, value|
+#     puts "#{value} #{index}"
+#   end
+#
+# end
+#
+# def summon_captain_planet(names)# code an argument here
+#   # Your code here
+#   names.map! do |name|
+#     name.capitalize + "!"
+#   end
+# end
+#
+# def long_planeteer_calls(array)
+#   array.any? { |call| call.length > 4 }
+# end
+#
+# def find_the_cheese(array)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   array.find do |cheese|
+#     cheese_types.include?(cheese)
+#   end
+#
+#
+# end
+
+def roll_call_dwarves(names)
+  collection.each do |name|
+    puts #{name}
   end
 
 end
 
-def summon_captain_planet(names)# code an argument here
-  # Your code here
-  names.map! do |name|
-    name.capitalize + "!"
-  end
+def summon_captain_planet
 end
 
-def long_planeteer_calls(array)
-  array.any? { |call| call.length > 4 }
+def long_planeteer_calls
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |cheese|
-    cheese_types.include?(cheese)
-  end
-  
-
+def find_the_cheese
 end
